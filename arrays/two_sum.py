2bfec43f25273939cebf5743abc35e8ea59c2152
zolajide <zainabolajide5@gmail.com>
2025-04-05 23:20:51 -0400

    Create two_sum.py

---
+++ arrays/two_sum.py
@@ -0,0 +1,8 @@
+# Difficulty: Easy
+
+def twoSum(nums, target):
+    lookup = {}
+    for i, num in enumerate(nums):
+        if target - num in lookup:
+            return [lookup[target - num], i]
+        lookup[num] = i
